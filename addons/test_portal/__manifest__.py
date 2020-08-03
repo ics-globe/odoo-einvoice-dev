25745a3b2a53520231d742ed1df6667142d88c3c
Xavier Morel <xmo@odoo.com>
2020-08-03 13:34:47 +0200

    [ADD] test_portal
    Bunch of tests for various tests of redirection of users on portal and
    non-portal objects, with or without access tokens.
    
    This mostly encodes existing behavior, but there are cases for which a
    more sensible behavior is encoded / expected, namely that an
    unidentified user opening a share link should go through the login
    flow and end up at the same place as a logged user, except for the
    sole case where:
    
    * the resource is portal-aware
    * the user is internal (not portal)
    * the user has access to the resource
    * the user is not logged
    * the user has an access token
    
    In this specific case, the access token overrides the possibility of
    logging in.

---
+++ addons/test_portal/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Portal Tests",
+    'category': 'Hidden/Tests',
+    'description': "Portal tests requiring specific testing models",
+    'depends': ['portal'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'license': 'LGPL-3',
+}
