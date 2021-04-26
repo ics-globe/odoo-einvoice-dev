b5420875812a6654e0163257385d9da9d82d33a0
Victor Feyens <vfe@odoo.com>
2021-04-26 19:00:59 +0200

    wip

--- addons/sale/wizard/sale_make_invoice_advance.py
+++ addons/sale/wizard/sale_make_invoice_advance.py
@@ -70,7 +70,7 @@ class SaleAdvancePaymentInv(models.TransientModel):
             return {'value': {'amount': amount}}
         return {}
 
-    def _prepare_invoice_values(self, order, name, amount, so_line):
+    def _prepare_invoice_values(self, order, so_line):
         invoice_vals = {
             'ref': order.client_order_ref,
             'move_type': 'out_invoice',
@@ -89,8 +89,8 @@ class SaleAdvancePaymentInv(models.TransientModel):
             'medium_id': order.medium_id.id,
             'source_id': order.source_id.id,
             'invoice_line_ids': [(0, 0, {
-                'name': name,
-                'price_unit': amount,
+                'name': self._get_order_name(order),
+                'price_unit': self._get_order_amount(order),
                 'quantity': 1.0,
                 'product_id': self.product_id.id,
                 'product_uom_id': so_line.product_uom.id,
@@ -103,25 +103,23 @@ class SaleAdvancePaymentInv(models.TransientModel):
 
         return invoice_vals
 
-    def _get_advance_details(self, order):
+    def _get_down_payment_amount(self, order):
+        if self.advance_payment_method == 'percentage':
+            return order.amount_untaxed * self.amount / 100
+        return self.fixed_amount
+
+    def _get_down_payment_name(self, order):
         context = {'lang': order.partner_id.lang}
         if self.advance_payment_method == 'percentage':
-            amount = order.amount_untaxed * self.amount / 100
-            name = _("Down payment of %s%%") % (self.amount)
+            name = _("Down payment of %s%%", self.amount)
         else:
-            amount = self.fixed_amount
             name = _('Down Payment')
         del context
 
-        return amount, name
+        return name
 
-    def _create_invoice(self, order, so_line, amount):
-        if (self.advance_payment_method == 'percentage' and self.amount <= 0.00) or (self.advance_payment_method == 'fixed' and self.fixed_amount <= 0.00):
-            raise UserError(_('The value of the down payment amount must be positive.'))
-
-        amount, name = self._get_advance_details(order)
-
-        invoice_vals = self._prepare_invoice_values(order, name, amount, so_line)
+    def _create_invoice(self, order, so_line):
+        invoice_vals = self._prepare_invoice_values(order, so_line)
 
         if order.fiscal_position_id:
             invoice_vals['fiscal_position_id'] = order.fiscal_position_id.id
@@ -133,23 +131,25 @@ class SaleAdvancePaymentInv(models.TransientModel):
                     subtype_id=self.env.ref('mail.mt_note').id)
         return invoice
 
-    def _prepare_so_line(self, order, analytic_tag_ids, tax_ids, amount):
-        context = {'lang': order.partner_id.lang}
-        so_values = {
-            'name': _('Down Payment: %s') % (time.strftime('%m %Y'),),
+    def _prepare_so_line_values(self, order):
+        product_taxes = self.product_id.taxes_id.filtered(
+            lambda t: t.company_id == order.company_id)
+        taxes = order.fiscal_position_id.map_tax(product_taxes)
+        amount = self._get_down_payment_amount(order)
+        name = self._get_down_payment_name(order) # Fixme why is the soline described with the downpayment time, while this is not the case for the invoice ?
+        return {
+            'name': _('Down Payment: %s', time.strftime('%m %Y')),
             'price_unit': amount,
             'product_uom_qty': 0.0,
             'order_id': order.id,
             'discount': 0.0,
             'product_uom': self.product_id.uom_id.id,
             'product_id': self.product_id.id,
-            'analytic_tag_ids': analytic_tag_ids,
-            'tax_id': [(6, 0, tax_ids)],
+            'analytic_tag_ids': [(6, 0, order.order_line.analytic_tag_ids)],
+            'tax_id': [(6, 0, taxes.ids)],
             'is_downpayment': True,
             'sequence': order.order_line and order.order_line[-1].sequence + 1 or 10,
         }
-        del context
-        return so_values
 
     def create_invoices(self):
         sale_orders = self.env['sale.order'].browse(self._context.get('active_ids', []))
@@ -163,23 +163,20 @@ class SaleAdvancePaymentInv(models.TransientModel):
                 self.product_id = self.env['product.product'].create(vals)
                 self.env['ir.config_parameter'].sudo().set_param('sale.default_deposit_product_id', self.product_id.id)
 
+            if self.product_id.invoice_policy != 'order':
+                raise UserError(_('The product used to invoice a down payment should have an invoice policy set to "Ordered quantities". Please update your deposit product to be able to create a deposit invoice.'))
+            if self.product_id.type != 'service':
+                raise UserError(_("The product used to invoice a down payment should be of type 'Service'. Please use another product or update this product."))
+
+            if (self.advance_payment_method == 'percentage' and self.amount <= 0.00) or (self.advance_payment_method == 'fixed' and self.fixed_amount <= 0.00):
+                raise UserError(_('The value of the down payment amount must be positive.'))
+
             sale_line_obj = self.env['sale.order.line']
             for order in sale_orders:
-                amount, name = self._get_advance_details(order)
-
-                if self.product_id.invoice_policy != 'order':
-                    raise UserError(_('The product used to invoice a down payment should have an invoice policy set to "Ordered quantities". Please update your deposit product to be able to create a deposit invoice.'))
-                if self.product_id.type != 'service':
-                    raise UserError(_("The product used to invoice a down payment should be of type 'Service'. Please use another product or update this product."))
-                taxes = self.product_id.taxes_id.filtered(lambda r: not order.company_id or r.company_id == order.company_id)
-                tax_ids = order.fiscal_position_id.map_tax(taxes).ids
-                analytic_tag_ids = []
-                for line in order.order_line:
-                    analytic_tag_ids = [(4, analytic_tag.id, None) for analytic_tag in line.analytic_tag_ids]
-
-                so_line_values = self._prepare_so_line(order, analytic_tag_ids, tax_ids, amount)
+                so_line_values = self._prepare_so_line_values(order)
                 so_line = sale_line_obj.create(so_line_values)
-                self._create_invoice(order, so_line, amount)
+                self._create_invoice(order, so_line)
+
         if self._context.get('open_invoices', False):
             return sale_orders.action_view_invoice()
         return {'type': 'ir.actions.act_window_close'}
@@ -193,4 +190,3 @@ class SaleAdvancePaymentInv(models.TransientModel):
             'taxes_id': [(6, 0, self.deposit_taxes_id.ids)],
             'company_id': False,
         }
-
