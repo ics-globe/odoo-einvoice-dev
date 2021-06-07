f63bb4c9e87c7b7be1458ffc2cabcfbc1d3a367d
yhu-odoo <yhu@odoo.com>
2021-06-07 10:37:49 +0200

    [IMP] purchase_stock: automatic use packaging in replenishment
    when using a buy route for replenishment, find if there is a packaging
    with the same qty as qty_multiple. If yes, use this packaging on
    purchase order.
    
    Task-2523550
    PR #71540

--- addons/purchase_stock/models/stock_orderpoint.py
+++ addons/purchase_stock/models/stock_orderpoint.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import api, models, _
 
 
 class StockWarehouseOrderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
+
+    @api.onchange('qty_multiple')
+    def _onchange_qty_multiple(self):
+        if not self.product_id or not self.qty_multiple or 'buy' not in self.rule_ids.mapped('action'):
+            return
+        purchase_packagings = self.product_id.packaging_ids.filtered('purchase')
+        if purchase_packagings:
+            packaging = purchase_packagings._find_suitable_product_packaging(self.qty_multiple, self.product_uom)
+            if not packaging:
+                msg = _("No packaging of quantity %d for this product. Available packagings: \n") % self.qty_multiple
+                msg += "\n".join([" - %s: %d" % (p.name, p.qty) for p in purchase_packagings])
+                return {'warning': {
+                    'title': _("Warning"),
+                    'message': msg,
+                }}
 
     def _get_qty_multiple_to_order(self):
         """ Calculates the minimum quantity that can be ordered according to the
