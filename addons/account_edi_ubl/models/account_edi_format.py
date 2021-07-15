a6d03d088839baf2666427e07224cb6479cddf90
Brice bib Bartoletti <bib@odoo.com>
2021-07-15 18:02:04 +0200

    [FIX] account_edi_ubl: fix double invoice creation
    Purpose:
    When importing a bill via the import feature using an xml, if the a ValidationError or a UserError is raised, cleanup the bill before propagating the Error.
    
    Context:
    Before this commit, if a bill was imported for the second time, it raised a ValidationError due to duplication but the account_move was still saved and persisted in the DB.
    
    closes opw-2601457

--- addons/account_edi_ubl/models/account_edi_format.py
+++ addons/account_edi_ubl/models/account_edi_format.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, models, fields, tools, _
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, float_repr
+from odoo import models, fields
+from odoo.exceptions import UserError, ValidationError
 from odoo.tests.common import Form
-from odoo.exceptions import UserError
 from odoo.osv import expression
 
 from pathlib import PureWindowsPath
@@ -54,126 +53,134 @@ class AccountEdiFormat(models.Model):
 
         namespaces = _get_ubl_namespaces()
 
-        with Form(invoice.with_context(account_predictive_bills_disable_prediction=True)) as invoice_form:
+        # During a Form.save(), if an Exception occurs, the record still persists in the DB
+        # thus we create the record, pass it to the Form and cleans it if required.
+        if not invoice:
+            invoice = self.env['account.move'].create({})
 
-            # Reference
-            elements = tree.xpath('//cbc:ID', namespaces=namespaces)
-            if elements:
-                invoice_form.ref = elements[0].text
-            elements = tree.xpath('//cbc:InstructionID', namespaces=namespaces)
-            if elements:
-                invoice_form.payment_reference = elements[0].text
+        invoice_form = Form(invoice.with_context(account_predictive_bills_disable_prediction=True))
+        # Reference
+        elements = tree.xpath('//cbc:ID', namespaces=namespaces)
+        if elements:
+            invoice_form.ref = elements[0].text
+        elements = tree.xpath('//cbc:InstructionID', namespaces=namespaces)
+        if elements:
+            invoice_form.payment_reference = elements[0].text
 
-            # Dates
-            elements = tree.xpath('//cbc:IssueDate', namespaces=namespaces)
-            if elements:
-                invoice_form.invoice_date = elements[0].text
-            elements = tree.xpath('//cbc:PaymentDueDate', namespaces=namespaces)
-            if elements:
-                invoice_form.invoice_date_due = elements[0].text
-            # allow both cbc:PaymentDueDate and cbc:DueDate
-            elements = tree.xpath('//cbc:DueDate', namespaces=namespaces)
-            invoice_form.invoice_date_due = invoice_form.invoice_date_due or elements and elements[0].text
+        # Dates
+        elements = tree.xpath('//cbc:IssueDate', namespaces=namespaces)
+        if elements:
+            invoice_form.invoice_date = elements[0].text
+        elements = tree.xpath('//cbc:PaymentDueDate', namespaces=namespaces)
+        if elements:
+            invoice_form.invoice_date_due = elements[0].text
+        # allow both cbc:PaymentDueDate and cbc:DueDate
+        elements = tree.xpath('//cbc:DueDate', namespaces=namespaces)
+        invoice_form.invoice_date_due = invoice_form.invoice_date_due or elements and elements[0].text
 
-            # Currency
-            elements = tree.xpath('//cbc:DocumentCurrencyCode', namespaces=namespaces)
-            currency_code = elements and elements[0].text or ''
-            currency = self.env['res.currency'].search([('name', '=', currency_code.upper())], limit=1)
-            if elements:
-                invoice_form.currency_id = currency
+        # Currency
+        elements = tree.xpath('//cbc:DocumentCurrencyCode', namespaces=namespaces)
+        currency_code = elements and elements[0].text or ''
+        currency = self.env['res.currency'].search([('name', '=', currency_code.upper())], limit=1)
+        if elements:
+            invoice_form.currency_id = currency
 
-            # Incoterm
-            elements = tree.xpath('//cbc:TransportExecutionTerms/cac:DeliveryTerms/cbc:ID', namespaces=namespaces)
-            if elements:
-                invoice_form.invoice_incoterm_id = self.env['account.incoterms'].search([('code', '=', elements[0].text)], limit=1)
+        # Incoterm
+        elements = tree.xpath('//cbc:TransportExecutionTerms/cac:DeliveryTerms/cbc:ID', namespaces=namespaces)
+        if elements:
+            invoice_form.invoice_incoterm_id = self.env['account.incoterms'].search([('code', '=', elements[0].text)], limit=1)
 
-            # Partner
-            partner_element = tree.xpath('//cac:AccountingSupplierParty/cac:Party', namespaces=namespaces)
-            if partner_element:
-                domains = []
-                partner_element = partner_element[0]
-                elements = partner_element.xpath('//cac:AccountingSupplierParty/cac:Party//cbc:Name', namespaces=namespaces)
-                if elements:
-                    partner_name = elements[0].text
-                    domains.append([('name', 'ilike', partner_name)])
+        # Partner
+        partner_element = tree.xpath('//cac:AccountingSupplierParty/cac:Party', namespaces=namespaces)
+        if partner_element:
+            domains = []
+            partner_element = partner_element[0]
+            elements = partner_element.xpath('//cac:AccountingSupplierParty/cac:Party//cbc:Name', namespaces=namespaces)
+            if elements:
+                partner_name = elements[0].text
+                domains.append([('name', 'ilike', partner_name)])
+            else:
+                partner_name = ''
+            elements = partner_element.xpath('//cac:AccountingSupplierParty/cac:Party//cbc:Telephone', namespaces=namespaces)
+            if elements:
+                partner_telephone = elements[0].text
+                domains.append([('phone', '=', partner_telephone), ('mobile', '=', partner_telephone)])
+            elements = partner_element.xpath('//cac:AccountingSupplierParty/cac:Party//cbc:ElectronicMail', namespaces=namespaces)
+            if elements:
+                partner_mail = elements[0].text
+                domains.append([('email', '=', partner_mail)])
+            elements = partner_element.xpath('//cac:AccountingSupplierParty/cac:Party//cbc:CompanyID', namespaces=namespaces)
+            if elements:
+                partner_id = elements[0].text
+                domains.append([('vat', 'like', partner_id)])
+
+            if domains:
+                partner = self.env['res.partner'].search(expression.OR(domains), limit=1)
+                if partner:
+                    invoice_form.partner_id = partner
+                    partner_name = partner.name
                 else:
-                    partner_name = ''
-                elements = partner_element.xpath('//cac:AccountingSupplierParty/cac:Party//cbc:Telephone', namespaces=namespaces)
-                if elements:
-                    partner_telephone = elements[0].text
-                    domains.append([('phone', '=', partner_telephone), ('mobile', '=', partner_telephone)])
-                elements = partner_element.xpath('//cac:AccountingSupplierParty/cac:Party//cbc:ElectronicMail', namespaces=namespaces)
-                if elements:
-                    partner_mail = elements[0].text
-                    domains.append([('email', '=', partner_mail)])
-                elements = partner_element.xpath('//cac:AccountingSupplierParty/cac:Party//cbc:CompanyID', namespaces=namespaces)
-                if elements:
-                    partner_id = elements[0].text
-                    domains.append([('vat', 'like', partner_id)])
+                    invoice_form.partner_id = self.env['res.partner']
 
+        # Lines
+        lines_elements = tree.xpath('//cac:InvoiceLine', namespaces=namespaces)
+        for eline in lines_elements:
+            with invoice_form.invoice_line_ids.new() as invoice_line_form:
+                # Product
+                elements = eline.xpath('cac:Item/cac:SellersItemIdentification/cbc:ID', namespaces=namespaces)
+                domains = []
+                if elements:
+                    product_code = elements[0].text
+                    domains.append([('default_code', '=', product_code)])
+                elements = eline.xpath('cac:Item/cac:StandardItemIdentification/cbc:ID[@schemeID=\'GTIN\']', namespaces=namespaces)
+                if elements:
+                    product_ean13 = elements[0].text
+                    domains.append([('barcode', '=', product_ean13)])
                 if domains:
-                    partner = self.env['res.partner'].search(expression.OR(domains), limit=1)
-                    if partner:
-                        invoice_form.partner_id = partner
-                        partner_name = partner.name
-                    else:
-                        invoice_form.partner_id = self.env['res.partner']
+                    product = self.env['product.product'].search(expression.OR(domains), limit=1)
+                    if product:
+                        invoice_line_form.product_id = product
 
-            # Lines
-            lines_elements = tree.xpath('//cac:InvoiceLine', namespaces=namespaces)
-            for eline in lines_elements:
-                with invoice_form.invoice_line_ids.new() as invoice_line_form:
-                    # Product
-                    elements = eline.xpath('cac:Item/cac:SellersItemIdentification/cbc:ID', namespaces=namespaces)
-                    domains = []
+                # Quantity
+                elements = eline.xpath('cbc:InvoicedQuantity', namespaces=namespaces)
+                quantity = elements and float(elements[0].text) or 1.0
+                invoice_line_form.quantity = quantity
+
+                # Price Unit
+                elements = eline.xpath('cac:Price/cbc:PriceAmount', namespaces=namespaces)
+                price_unit = elements and float(elements[0].text) or 0.0
+                elements = eline.xpath('cbc:LineExtensionAmount', namespaces=namespaces)
+                line_extension_amount = elements and float(elements[0].text) or 0.0
+                invoice_line_form.price_unit = price_unit or line_extension_amount / invoice_line_form.quantity or 0.0
+
+                # Name
+                elements = eline.xpath('cac:Item/cbc:Description', namespaces=namespaces)
+                if elements and elements[0].text:
+                    invoice_line_form.name = elements[0].text
+                    invoice_line_form.name = invoice_line_form.name.replace('%month%', str(fields.Date.to_date(invoice_form.invoice_date).month))  # TODO: full name in locale
+                    invoice_line_form.name = invoice_line_form.name.replace('%year%', str(fields.Date.to_date(invoice_form.invoice_date).year))
+                else:
+                    invoice_line_form.name = "%s (%s)" % (partner_name or '', invoice_form.invoice_date)
+
+                # Taxes
+                taxes_elements = eline.xpath('cac:TaxTotal/cac:TaxSubtotal', namespaces=namespaces)
+                invoice_line_form.tax_ids.clear()
+                for etax in taxes_elements:
+                    elements = etax.xpath('cbc:Percent', namespaces=namespaces)
                     if elements:
-                        product_code = elements[0].text
-                        domains.append([('default_code', '=', product_code)])
-                    elements = eline.xpath('cac:Item/cac:StandardItemIdentification/cbc:ID[@schemeID=\'GTIN\']', namespaces=namespaces)
-                    if elements:
-                        product_ean13 = elements[0].text
-                        domains.append([('barcode', '=', product_ean13)])
-                    if domains:
-                        product = self.env['product.product'].search(expression.OR(domains), limit=1)
-                        if product:
-                            invoice_line_form.product_id = product
+                        tax = self.env['account.tax'].search([
+                            ('company_id', '=', self.env.company.id),
+                            ('amount', '=', float(elements[0].text)),
+                            ('type_tax_use', '=', invoice_form.journal_id.type),
+                        ], order='sequence ASC', limit=1)
+                        if tax:
+                            invoice_line_form.tax_ids.add(tax)
 
-                    # Quantity
-                    elements = eline.xpath('cbc:InvoicedQuantity', namespaces=namespaces)
-                    quantity = elements and float(elements[0].text) or 1.0
-                    invoice_line_form.quantity = quantity
-
-                    # Price Unit
-                    elements = eline.xpath('cac:Price/cbc:PriceAmount', namespaces=namespaces)
-                    price_unit = elements and float(elements[0].text) or 0.0
-                    elements = eline.xpath('cbc:LineExtensionAmount', namespaces=namespaces)
-                    line_extension_amount = elements and float(elements[0].text) or 0.0
-                    invoice_line_form.price_unit = price_unit or line_extension_amount / invoice_line_form.quantity or 0.0
-
-                    # Name
-                    elements = eline.xpath('cac:Item/cbc:Description', namespaces=namespaces)
-                    if elements and elements[0].text:
-                        invoice_line_form.name = elements[0].text
-                        invoice_line_form.name = invoice_line_form.name.replace('%month%', str(fields.Date.to_date(invoice_form.invoice_date).month))  # TODO: full name in locale
-                        invoice_line_form.name = invoice_line_form.name.replace('%year%', str(fields.Date.to_date(invoice_form.invoice_date).year))
-                    else:
-                        invoice_line_form.name = "%s (%s)" % (partner_name or '', invoice_form.invoice_date)
-
-                    # Taxes
-                    taxes_elements = eline.xpath('cac:TaxTotal/cac:TaxSubtotal', namespaces=namespaces)
-                    invoice_line_form.tax_ids.clear()
-                    for etax in taxes_elements:
-                        elements = etax.xpath('cbc:Percent', namespaces=namespaces)
-                        if elements:
-                            tax = self.env['account.tax'].search([
-                                ('company_id', '=', self.env.company.id),
-                                ('amount', '=', float(elements[0].text)),
-                                ('type_tax_use', '=', invoice_form.journal_id.type),
-                            ], order='sequence ASC', limit=1)
-                            if tax:
-                                invoice_line_form.tax_ids.add(tax)
-
-        invoice = invoice_form.save()
+        try:
+            invoice = invoice_form.save()
+        except (ValidationError, UserError):
+            invoice.unlink()
+            raise
 
         # Regenerate PDF
         attachments = self.env['ir.attachment']
