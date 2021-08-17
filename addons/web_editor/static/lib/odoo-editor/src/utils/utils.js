3e0ddd436fcf3823426eb3780aa9ed05499945aa
Nicolas Bayet <nby@odoo.com>
2021-08-17 11:59:49 +0200

    clean

--- addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/lib/odoo-editor/src/utils/utils.js
@@ -1888,6 +1888,6 @@ export function throttle(func, wait) {
         }
     };
 }
-export function peek() {
-    return arr => arr[arr.length - 1];
+export function peek(arr) {
+    return arr[arr.length - 1];
 }
