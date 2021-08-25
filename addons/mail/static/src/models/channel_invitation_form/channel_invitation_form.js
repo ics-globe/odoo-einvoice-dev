odoo-dev/odoo

[IMP] mail: add group chat feature
task-2360934

addons/mail/static/src/models/channel_invitation_form/channel_invitation_form.js
@@ -34,14 +34,32 @@ function factory(dependencies) {
          * @param {MouseEvent} ev
          */
         async onClickInvite(ev) {
-            await this.env.services.rpc(({
-                model: 'mail.channel',
-                method: 'add_members',
-                args: [this.thread.id],
-                kwargs: {
-                    partner_ids: this.selectedPartners.map(partner => partner.id),
-                },
-            }));
+            if (this.thread.channel_type === 'chat') {
+                const channelData = await this.env.services.rpc(({
+                    model: 'mail.channel',
+                    method: 'group_chat_create',
+                    kwargs: {
+                        partners_to: [...new Set([
+                            this.env.messaging.currentPartner.id,
+                            ...this.thread.members.map(member => member.id),
+                            ...this.selectedPartners.map(partner => partner.id),
+                        ])],
+                    },
+                }));
+                const channel = this.env.models['mail.thread'].insert(
+                    this.env.models['mail.thread'].convertData(channelData)
+                );
+                channel.open();
+            } else {
+                await this.env.services.rpc(({
+                    model: 'mail.channel',
+                    method: 'add_members',
+                    args: [this.thread.id],
+                    kwargs: {
+                        partner_ids: this.selectedPartners.map(partner => partner.id),
+                    },
+                }));
+            }
             this.update({
                 searchTerm: "",
                 selectedPartners: unlinkAll(),
@@ -148,8 +166,14 @@ function factory(dependencies) {
          * @returns {string}
          */
         _computeInviteButtonText() {
-            if (!this.thread || this.thread.channel_type !== 'channel') {
+            if (!this.thread) {
                 return clear();
+            }
+            switch (this.thread.channel_type) {
+                case 'chat':
+                    return this.env._t("Create group chat");
+                case 'group':
+                    return this.env._t("Invite to group chat");
             }
             return this.env._t("Invite to Channel");
         }

addons/mail/static/src/models/messaging/messaging.js
@@ -58,6 +58,19 @@ function factory(dependencies) {
             }
         }
 
+         /**
+          * @param {mail.partner[]} partners
+          * @returns {mail.thread|undefined}
+          */
+        async createGroupChat(partners) {
+            const chat = await this.async(() =>
+                this.env.models['mail.thread'].performRpcCreateGroupChat({
+                    partnerIds: partners.map(partner => partner.id),
+                })
+            );
+            return chat;
+        }
+
         /**
          * Opens a chat with the provided person and returns it.
          *
@@ -97,6 +110,22 @@ function factory(dependencies) {
                 // be closed to ensure the visibility of the view
                 this.env.messaging.messagingMenu.close();
             }
+        }
+
+        /**
+         * Opens a group chat with the provided persons and returns it.
+         *
+         * @param {mail.partner[]} partners forwarded to @see `createGroupChat()`
+         * @param {Object} [options] forwarded to @see `mail.thread:open()`
+         * @returns {mail.thread|undefined}
+         */
+        async openGroupChat(partners, options) {
+            const chat = await this.async(() => this.createGroupChat(partners));
+            if (!chat) {
+                return;
+            }
+            await this.async(() => chat.open(options));
+            return chat;
         }
 
         /**

addons/mail/static/src/models/thread/thread.js
@@ -520,6 +520,39 @@ function factory(dependencies) {
         }
 
         /**
+         * Performs the `group_chat_create` RPC on `mail.channel`.
+         *
+         *
+         * @static
+         * @param {Object} param0
+         * @param {integer[]} param0.partnerIds
+         * @param {boolean} [param0.pinForCurrentPartner]
+         * @returns {mail.thread|undefined} the created or existing chat
+         */
+        static async performRpcCreateGroupChat({ partnerIds, pinForCurrentPartner }) {
+            const device = this.env.messaging.device;
+            const data = await this.env.services.rpc({
+                model: 'mail.channel',
+                method: 'group_chat_create',
+                kwargs: {
+                    context: Object.assign({}, this.env.session.user_content, {
+                        // optimize the return value by avoiding useless queries
+                        // in non-mobile devices
+                        isMobile: device.isMobile,
+                    }),
+                    partners_to: partnerIds,
+                    pin: pinForCurrentPartner,
+                },
+            });
+            if (!data) {
+                return;
+            }
+            return this.env.models['mail.thread'].insert(
+                this.env.models['mail.thread'].convertData(data)
+            );
+        }
+
+        /**
          * Performs the `execute_command` RPC on `mail.channel`.
          *
          * @static
@@ -1167,6 +1200,9 @@ function factory(dependencies) {
             if (this.channel_type === 'chat' && this.correspondent) {
                 return this.custom_channel_name || this.correspondent.nameOrDisplayName;
             }
+            if (this.channel_type === 'group') {
+                return this.name || this.members.map(partner => partner.nameOrDisplayName).join(', ');
+            }
             return this.name;
         }
 
@@ -1183,10 +1219,7 @@ function factory(dependencies) {
          * @returns {boolean}
          */
         _computeHasInviteFeature() {
-            if (this.model !== 'mail.channel') {
-                return false;
-            }
-            return this.channel_type === 'channel';
+            return this.model === 'mail.channel';
         }
 
         /**
@@ -1207,7 +1240,7 @@ function factory(dependencies) {
         _computeIsChannelRenamable() {
             return (
                 this.model === 'mail.channel' &&
-                ['chat', 'channel'].includes(this.channel_type)
+                ['chat', 'channel', 'group'].includes(this.channel_type)
             );
         }
 

ics-globe/odoo-einvoice-dev

addons/mail/static/src/models/channel_invitation_form/channel_invitation_form.js
@@ -34,14 +34,32 @@ function factory(dependencies) {
          * @param {MouseEvent} ev
          */
         async onClickInvite(ev) {
-            await this.env.services.rpc(({
-                model: 'mail.channel',
-                method: 'add_members',
-                args: [this.thread.id],
-                kwargs: {
-                    partner_ids: this.selectedPartners.map(partner => partner.id),
-                },
-            }));
+            if (this.thread.channel_type === 'chat') {
+                const channelData = await this.env.services.rpc(({
+                    model: 'mail.channel',
+                    method: 'group_chat_create',
+                    kwargs: {
+                        partners_to: [...new Set([
+                            this.messaging.currentPartner.id,
+                            ...this.thread.members.map(member => member.id),
+                            ...this.selectedPartners.map(partner => partner.id),
+                        ])],
+                    },
+                }));
+                const channel = this.messaging.models['mail.thread'].insert(
+                    this.messaging.models['mail.thread'].convertData(channelData)
+                );
+                channel.open();
+            } else {
+                await this.env.services.rpc(({
+                    model: 'mail.channel',
+                    method: 'add_members',
+                    args: [this.thread.id],
+                    kwargs: {
+                        partner_ids: this.selectedPartners.map(partner => partner.id),
+                    },
+                }));
+            }
             this.update({
                 searchTerm: "",
                 selectedPartners: unlinkAll(),
@@ -148,8 +166,14 @@ function factory(dependencies) {
          * @returns {string}
          */
         _computeInviteButtonText() {
-            if (!this.thread || this.thread.channel_type !== 'channel') {
+            if (!this.thread) {
                 return clear();
+            }
+            switch (this.thread.channel_type) {
+                case 'chat':
+                    return this.env._t("Create group chat");
+                case 'group':
+                    return this.env._t("Invite to group chat");
             }
             return this.env._t("Invite to Channel");
         }

addons/mail/static/src/models/messaging/messaging.js
@@ -60,6 +60,19 @@ function factory(dependencies) {
             }
         }
 
+         /**
+          * @param {mail.partner[]} partners
+          * @returns {mail.thread|undefined}
+          */
+        async createGroupChat(partners) {
+            const chat = await this.async(() =>
+                this.messaging.models['mail.thread'].performRpcCreateGroupChat({
+                    partnerIds: partners.map(partner => partner.id),
+                })
+            );
+            return chat;
+        }
+
         /**
          * Opens a chat with the provided person and returns it.
          *
@@ -99,6 +112,22 @@ function factory(dependencies) {
                 // be closed to ensure the visibility of the view
                 this.messaging.messagingMenu.close();
             }
+        }
+
+        /**
+         * Opens a group chat with the provided persons and returns it.
+         *
+         * @param {mail.partner[]} partners forwarded to @see `createGroupChat()`
+         * @param {Object} [options] forwarded to @see `mail.thread:open()`
+         * @returns {mail.thread|undefined}
+         */
+        async openGroupChat(partners, options) {
+            const chat = await this.async(() => this.createGroupChat(partners));
+            if (!chat) {
+                return;
+            }
+            await this.async(() => chat.open(options));
+            return chat;
         }
 
         /**

addons/mail/static/src/models/thread/thread.js
@@ -543,6 +543,39 @@ function factory(dependencies) {
         }
 
         /**
+         * Performs the `group_chat_create` RPC on `mail.channel`.
+         *
+         *
+         * @static
+         * @param {Object} param0
+         * @param {integer[]} param0.partnerIds
+         * @param {boolean} [param0.pinForCurrentPartner]
+         * @returns {mail.thread|undefined} the created or existing chat
+         */
+        static async performRpcCreateGroupChat({ partnerIds, pinForCurrentPartner }) {
+            const device = this.messaging.device;
+            const data = await this.env.services.rpc({
+                model: 'mail.channel',
+                method: 'group_chat_create',
+                kwargs: {
+                    context: Object.assign({}, this.env.session.user_content, {
+                        // optimize the return value by avoiding useless queries
+                        // in non-mobile devices
+                        isMobile: device.isMobile,
+                    }),
+                    partners_to: partnerIds,
+                    pin: pinForCurrentPartner,
+                },
+            });
+            if (!data) {
+                return;
+            }
+            return this.messaging.models['mail.thread'].insert(
+                this.messaging.models['mail.thread'].convertData(data)
+            );
+        }
+
+        /**
          * Performs the `message_post` RPC on given threadModel.
          *
          * @static
@@ -1209,6 +1242,9 @@ function factory(dependencies) {
             if (this.channel_type === 'chat' && this.correspondent) {
                 return this.custom_channel_name || this.correspondent.nameOrDisplayName;
             }
+            if (this.channel_type === 'group') {
+                return this.name || this.members.map(partner => partner.nameOrDisplayName).join(', ');
+            }
             return this.name;
         }
 
@@ -1225,10 +1261,7 @@ function factory(dependencies) {
          * @returns {boolean}
          */
         _computeHasInviteFeature() {
-            if (this.model !== 'mail.channel') {
-                return false;
-            }
-            return this.channel_type === 'channel';
+            return this.model === 'mail.channel';
         }
 
         /**
@@ -1257,7 +1290,7 @@ function factory(dependencies) {
         _computeIsChannelRenamable() {
             return (
                 this.model === 'mail.channel' &&
-                ['chat', 'channel'].includes(this.channel_type)
+                ['chat', 'channel', 'group'].includes(this.channel_type)
             );
         }
 