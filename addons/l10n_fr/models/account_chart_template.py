f1f7944cb18d5b3c4be21ef7a90b7f83d9da5fc4
dbkosky <dako@odoo.com>
2021-08-26 12:01:31 +0200

    [IMP] l10n_fr_reports: Round lines of tax report and closing entries to the nearest euro
    French tax reports require that each line is rounded to the nearest euro.
    This requires that a line is added to the closing entries to account for the
    differences from rounding. The line requires a couple of default
    accounts for the profit/loss from rounding to be applied.
    
    These accounts for profit/loss are by default 758000/658000 and are inherited in the
    chart of accounts for l10n_fr (you will only need them if you're doing
    french tax reports). The fields are also added to res_company for
    l10n_fr, non french companies can still file french tax reports, so if
    they are filing french tax reports without these accounts then they'll
    need to specify them in settings
    
    task-2610752

--- addons/l10n_fr/models/account_chart_template.py
+++ addons/l10n_fr/models/account_chart_template.py
@@ -7,6 +7,31 @@ from odoo import api, models, fields
 class AccountChartTemplate(models.Model):
     _inherit = 'account.chart.template'
 
+    l10n_fr_rounding_difference_loss_account_id = fields.Many2one(
+        'account.account.template',
+        string="Loss Account for Rounding Differences",
+        help="Account used for losses from rounding the lines of French tax returns",
+    )
+    l10n_fr_rounding_difference_profit_account_id = fields.Many2one(
+        'account.account.template',
+        string="Profit Account for Rounding Differences",
+        help="Account used for profits from rounding the lines of French tax returns",
+    )
+
+    def _load_company_accounts(self, account_ref, company):
+        super()._load_company_accounts(account_ref, company)
+
+        # Add the fields that will contain the accounts used for the
+        # profit/loss from rounding lines of the french tax report
+        company.write({
+            field: account_ref[self[field]]
+            for field in (
+                'l10n_fr_rounding_difference_loss_account_id',
+                'l10n_fr_rounding_difference_profit_account_id',
+            )
+            if account_ref.get(self[field])
+        })
+
     @api.model
     def _prepare_all_journals(self, acc_template_ref, company, journals_dict=None):
         journal_data = super(AccountChartTemplate, self)._prepare_all_journals(
