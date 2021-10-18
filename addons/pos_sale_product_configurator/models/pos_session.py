d3895bf253b1378774bbdbd25fbc36bc74470f58
Joseph Caburnay <jcb@odoo.com>
2021-10-18 21:44:02 +0200

    fix

--- addons/pos_sale_product_configurator/models/pos_session.py
+++ addons/pos_sale_product_configurator/models/pos_session.py
@@ -11,8 +11,3 @@ class PosSession(models.Model):
         meta = super()._meta_product_product()
         meta["fields"].append("optional_product_ids")
         return meta
-
-    def _meta_pos_config(self):
-        meta = super()._meta_pos_config()
-        meta["fields"].append("iface_open_product_info")
-        return meta
