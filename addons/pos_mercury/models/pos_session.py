c2e9e4c23a84fb206ced8e43d6699ce83bee5014
Joseph Caburnay <jcb@odoo.com>
2021-10-18 15:45:25 +0200

    load_fields of pos_mercury

---
+++ addons/pos_mercury/models/pos_session.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+
+class PosSession(models.Model):
+    _inherit = "pos.session"
+
+    def _meta_pos_payment_method(self):
+        meta = super()._meta_pos_payment_method()
+        meta["fields"].append("pos_mercury_config_id")
+        return meta
