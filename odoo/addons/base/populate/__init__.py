3153d81d5388e9b3899ecbeda7c8b390d1db1d53
Thibault Delavallée <tde@odoo.com>
2021-10-28 15:53:42 +0200

    tmp: stop crashing base please

--- odoo/addons/base/populate/__init__.py
+++ odoo/addons/base/populate/__init__.py
@@ -1,4 +1,4 @@
-from . import res_partner
-from . import res_company
-from . import res_currency
-from . import res_user
+# from . import res_partner
+# from . import res_company
+# from . import res_currency
+# from . import res_user
