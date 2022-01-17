9ed56e6bbc72336c16de5016cc4d72087adc54a5
Benoit Socias <bso@odoo.com>
2022-01-17 17:04:03 +0100

    wip - default job description

--- addons/website_hr_recruitment/models/hr_recruitment.py
+++ addons/website_hr_recruitment/models/hr_recruitment.py
@@ -55,10 +55,15 @@ class Job(models.Model):
         'website.searchable.mixin',
     ]
 
+    def _get_default_description(self):
+        default_description = self.env.ref("website_hr_recruitment.default_description", raise_if_not_found=False)
+        return (default_description._render() if default_description else "")
+
     def _get_default_website_description(self):
         default_description = self.env.ref("website_hr_recruitment.default_website_description", raise_if_not_found=False)
         return (default_description._render() if default_description else "")
 
+    description = fields.Html(string='Job Description', translate=html_translate, sanitize_attributes=False, default=_get_default_description, prefetch=False, sanitize_form=False)
     website_published = fields.Boolean(help='Set if the application is published on the website of the company.')
     website_description = fields.Html('Website description', translate=html_translate, sanitize_attributes=False, default=_get_default_website_description, prefetch=False, sanitize_form=False)
 
