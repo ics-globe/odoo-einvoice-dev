5cf3ae365ebb64d2bf382f1a26e343b93f838abb
Nicolas Bayet <nby@odoo.com>
2022-03-17 12:07:03 +0100

    [FIX] web_editor: resolve shortcut conflict of link dialog & command palette
    Whenever the user hit ctrl+k when being on an html field, both the
    link dialog and the command palette opened.
    
    In order to resolve the conflict, now ctrl+k open the command palette
    with the first entry being the command to open the linktool and
    a subsequent hit to ctrl+k when the command palette is open to open
    the link dialog.
    
    Task-2738457
    
    Forward-Port-Of: #89940
    Forward-Port-Of: #82994

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -89,6 +89,7 @@ const Wysiwyg = Widget.extend({
         // more heuristics to bypass the limitation.
         this._isOnline = true;
         this._signalOnline = this._signalOnline.bind(this);
+        Wysiwyg.activeWysiwygs.add(this);
     },
     /**
      *
@@ -632,6 +633,7 @@ const Wysiwyg = Widget.extend({
      * @override
      */
     destroy: function () {
+        Wysiwyg.activeWysiwygs.delete(this);
         if (this._collaborationChannelName) {
             Wysiwyg.activeCollaborationChannelNames.delete(this._collaborationChannelName);
         }
@@ -948,6 +950,9 @@ const Wysiwyg = Widget.extend({
             await this.snippetsMenu.cleanForSave();
         }
     },
+    isSelectionInEditable: function () {
+        return this.odooEditor.isSelectionInEditable();
+    },
     /**
      * Start or resume the Odoo field changes muation observers.
      *
@@ -1037,6 +1042,19 @@ const Wysiwyg = Widget.extend({
         for (let observerData of this.odooFieldObservers) {
             observerData.observer.disconnect();
         }
+    },
+    /**
+     * Open the link tools or the image link tool depending on the selection.
+     */
+    openLinkToolsFromSelection() {
+        const targetEl = this.odooEditor.document.getSelection().getRangeAt(0).startContainer;
+        // Link tool is different if the selection is an image or a text.
+        if (targetEl instanceof HTMLElement
+                && (targetEl.tagName === 'IMG' || targetEl.querySelectorAll('img').length === 1)) {
+            core.bus.trigger('activate_image_link_tool');
+            return;
+        }
+        this.toggleLinkTools();
     },
     /**
      * Toggle the Link tools/dialog to edit links. If a snippet menu is present,
@@ -1233,6 +1251,9 @@ const Wysiwyg = Widget.extend({
             fragment.append(element);
             return this.odooEditor.execCommand('insertFragment', fragment);
         }
+    },
+    getInSelection(selector) {
+        return getInSelection(this.odooEditor.document, selector);
     },
 
     //--------------------------------------------------------------------------
@@ -1533,17 +1554,11 @@ const Wysiwyg = Widget.extend({
      * Handle custom keyboard shortcuts.
      */
     _handleShortcuts: function (e) {
+        const options = this._editorOptions();
         // Open the link tool when CTRL+K is pressed.
-        if (e && e.key === 'k' && (e.ctrlKey || e.metaKey)) {
+        if (options.bindLinkTool && e && e.key === 'k' && (e.ctrlKey || e.metaKey)) {
             e.preventDefault();
-            const targetEl = this.odooEditor.document.getSelection().getRangeAt(0).startContainer;
-            // Link tool is different if the selection is an image or a text.
-            if (targetEl instanceof HTMLElement
-                    && (targetEl.tagName === 'IMG' || targetEl.querySelectorAll('img').length === 1)) {
-                core.bus.trigger('activate_image_link_tool');
-                return;
-            }
-            this.toggleLinkTools();
+            this.openLinkToolsFromSelection();
         }
         // Override selectAll (CTRL+A) to restrict it to the editable zone / current snippet and prevent traceback.
         if (e && e.key === 'a' && (e.ctrlKey || e.metaKey)) {
@@ -2122,6 +2137,7 @@ const Wysiwyg = Widget.extend({
     }
 });
 Wysiwyg.activeCollaborationChannelNames = new Set();
+Wysiwyg.activeWysiwygs = new Set();
 //--------------------------------------------------------------------------
 // Public helper
 //--------------------------------------------------------------------------
