1b1b5ce1e306cdf12cc7f755d1c1fcafaa2bdb7b
Nemael <mapa@odoo.com>
2022-03-22 10:45:06 +0100

    [MOV] im_livechat: move public_livechat.xml into public_livechat folder
    The public_livechat.xml file should be moved into `legacy/public_livechat`
    folder, to maintain structure.
    public_livechat.xml is moved to public_livechat/public_livechat.xml
    
    The sub-folder `public_livechat` has been created
    for better organization and to suit the
    new file structure of im_livechat
    
    Task-2212347

--- addons/im_livechat/controllers/main.py
+++ addons/im_livechat/controllers/main.py
@@ -32,7 +32,7 @@ class LivechatController(http.Controller):
     def load_templates(self, **kwargs):
         base_url = request.httprequest.base_url
         templates = [
-            'im_livechat/static/src/legacy/public_livechat.xml',
+            'im_livechat/static/src/legacy/public_livechat/public_livechat.xml',
         ]
         return [tools.file_open(tmpl, 'rb').read() for tmpl in templates]
 
