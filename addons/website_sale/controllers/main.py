8f235cbae3c61c0a4146a46a315892953ab2e56b
Bastien Dosimont <doba@odoo.com>
2022-03-23 15:34:06 +0100

    [ADD] website_sale_onsite : Asking to pay/ship at physical store
    Task-id : 2791237
    PR : https://github.com/odoo/odoo/pull/87636
    
    Allows customers to see a list of delivery carriers as shops in the website page;
    A delivery carrier may now be an onsite one, which means the client comes to the corresponding store to pickup the products.
    
    Also adds a payment acquirer based on wire transfer, for paying on site.
    The customer can only pay on site if he also comes to pick the product on site
    
    Notes :
    
    - If a warehouse is set on a delivery carrier, the sales order will take it's address.

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -1240,6 +1240,13 @@ class WebsiteSale(http.Controller):
 
 class PaymentPortal(payment_portal.PaymentPortal):
 
+    def _validate_transaction_for_order(self, transaction, sale_order_id):
+        """
+        Perform final checks against the transaction & sale_order.
+        Override me to apply payment unrelated checks & processing
+        """
+        return
+
     @http.route(
         '/shop/payment/transaction/<int:order_id>', type='json', auth='public', website=True
     )
@@ -1277,5 +1284,7 @@ class PaymentPortal(payment_portal.PaymentPortal):
         if last_tx:
             PaymentPostProcessing.remove_transactions(last_tx)
         request.session['__website_sale_last_tx_id'] = tx_sudo.id
+
+        self._validate_transaction_for_order(tx_sudo, order_id)
 
         return tx_sudo._get_processing_values()
