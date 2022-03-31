odoo/odoo

[REF] models: refactor fields_view_get, load_views
The global goal is to rework `load_views` so it sends less/redundant
information and it gets called less by the webclient,
and to improve the performance in general

- `_fields_view_get`
    - Renamed `_view_get` (as the goal is to only return the view, and no
    longer the fields information, see below).
    - Arguments `toolbar=False, submenu=False` converted to a kwargs
    `**options`. The rationale is that `submenu` was already no longer
    used (deprecated) and the `mobile` options is introduced.
    The mobile options is necessary to tell the server to send the
    mobile views for x2many fields (kanban instead of tree). See below.
    Instead of adding a new argument each time we add a new option to
    `_fields_view_get`, it seems wiser to have a kwargs `**options`
    to avoid to re-write all overrides each time a new option is
    introduced.
    - Formely returned the arch in text and some of the view information
    in a dict.
    Now returns the view architecture as an etree node,
    and the view as a browse record
    The rationale is that all overrides of `_fields_view_get` were about
    modifying the arch only (e.g. changing the address
    format/re-organizing the address related field nodes of the
    partner according to the company country).
    To do so, all these overrides were doing `etree.fromstring` to parse
    the arch which was sent in text to convert it to an etree, then
    operations were done on the etree, and then `etree.tostring` was
    called to convert back the arch to string.
    With this change of signature to send the arch as an etree,
    all these back and forth `etree.fromstring` -> `etree.tostring` are
    avoided, allowing some performance gain and less code in the end.

- `fields_view_get`
    - Renamed `view_get` (as the goal is to only return the view, and no
    longer the fields information, read below).
    - Arguments `toolbar=False, submenu=False` converted 

addons/hr/models/res_users.py
@@ -168,7 +168,7 @@ class User(models.Model):
         return super().SELF_WRITEABLE_FIELDS + HR_WRITABLE_FIELDS
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+    def view_get(self, view_id=None, view_type='form', **options):
         # When the front-end loads the views it gets the list of available fields
         # for the user (according to its access rights). Later, when the front-end wants to
         # populate the view with data, it only asks to read those available fields.
@@ -181,11 +181,11 @@ class User(models.Model):
         original_user = self.env.user
         if profile_view and view_id == profile_view.id:
             self = self.with_user(SUPERUSER_ID)
-        result = super(User, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        result = super(User, self).view_get(view_id=view_id, view_type=view_type, **options)
         # Due to using the SUPERUSER the result will contain action that the user may not have access too
         # here we filter out actions that requires special implicit rights to avoid having unusable actions
         # in the dropdown menu.
-        if toolbar and self.env.user != original_user:
+        if options.get('toolbar') and self.env.user != original_user:
             self = self.with_user(original_user.id)
             if not self.user_has_groups("base.group_erp_manager"):
                 change_password_action = self.env.ref("base.change_password_wizard_action")

addons/hr/tests/test_self_user_access.py
@@ -3,6 +3,7 @@
 
 from collections import OrderedDict
 from itertools import chain
+from lxml import etree
 
 from odoo.addons.hr.tests.common import TestHrCommon
 from odoo.tests import new_test_user, tagged, Form
@@ -20,8 +21,8 @@ class TestSelfAccessProfile(TestHrCommon):
             'user_id': james.id,
         })
         view = self.env.ref('hr.res_users_view_form_profile')
-        view_infos = james.fields_view_get(view_id=view.id)
-        fields = view_infos['fields'].keys()
+        view_infos = james.view_get(view_id=view.id)
+        fields = [el.get('name') for el in etree.fromstring(view_infos['arch']).xpath('//field[not(ancestor::field)]')]
         james.read(fields)
 
     def test_readonly_fields(self):
@@ -35,12 +36,12 @@ class TestSelfAccessProfile(TestHrCommon):
         })
 
         view = self.env.ref('hr.res_users_view_form_profile')
-        view_infos = james.fields_view_get(view_id=view.id)
-
+        fields = james._fields
+        view_infos = james.view_get(view_id=view.id)
         employee_related_fields = {
-            field_name
-            for field_name, field_attrs in view_infos['fields'].items()
-            if field_attrs.get('related', (None,))[0] == 'employee_id'
+            el.get('name')
+            for el in etree.fromstring(view_infos['arch']).xpath('//field[not(ancestor::field)]')
+            if fields[el.get('name')].related and fields[el.get('name')].related.split('.')[0] == 'employee_id'
         }
 
         form = Form(james, view=view)
@@ -65,16 +66,16 @@ class TestSelfAccessProfile(TestHrCommon):
             all_groups |= self.env.ref(xml_id.strip())
         user_all_groups = new_test_user(self.env, groups='base.group_user', login='hel', name='God')
         user_all_groups.write({'groups_id': [(4, group.id, False) for group in all_groups]})
-        view_infos = self.env['res.users'].with_user(user_all_groups).fields_view_get(view_id=view.id)
-        full_fields = view_infos['fields']
+        view_infos = self.env['res.users'].with_user(user_all_groups).view_get(view_id=view.id)
+        full_fields = [el.get('name') for el in etree.fromstring(view_infos['arch']).xpath('//field[not(ancestor::field)]')]
 
         # Now check the view for a simple user
         user = new_test_user(self.env, login='gro', name='Grouillot')
-        view_infos = self.env['res.users'].with_user(user).fields_view_get(view_id=view.id)
-        fields = view_infos['fields']
+        view_infos = self.env['res.users'].with_user(user).view_get(view_id=view.id)
+        fields = [el.get('name') for el in etree.fromstring(view_infos['arch']).xpath('//field[not(ancestor::field)]')]
 
         # Compare both
-        self.assertEqual(full_fields.keys(), fields.keys(), "View fields should not depend on user's groups")
+        self.assertEqual(full_fields, fields, "View fields should not depend on user's groups")
 
     def test_access_my_profile_toolbar(self):
         """ A simple user shouldn't have the possibilities to see the 'Change Password' action"""
@@ -85,7 +86,7 @@ class TestSelfAccessProfile(TestHrCommon):
             'user_id': james.id,
         })
         view = self.env.ref('hr.res_users_view_form_profile')
-        available_actions = james.fields_view_get(view_id=view.id, toolbar=True)['toolbar']['action']
+        available_actions = james.view_get(view_id=view.id, toolbar=True)['toolbar']['action']
         change_password_action = self.env.ref("base.change_password_wizard_action")
 
         self.assertFalse(any(x['id'] == change_password_action.id for x in available_actions))
@@ -98,7 +99,7 @@ class TestSelfAccessProfile(TestHrCommon):
             'user_id': john.id,
         })
         view = self.env.ref('hr.res_users_view_form_profile')
-        available_actions = john.fields_view_get(view_id=view.id, toolbar=True)['toolbar']['action']
+        available_actions = john.view_get(view_id=view.id, toolbar=True)['toolbar']['action']
         self.assertTrue(any(x['id'] == change_password_action.id for x in available_actions))
 
 

addons/hr_recruitment/models/hr_recruitment.py
@@ -416,10 +416,10 @@ class Applicant(models.Model):
         return nocontent_body % nocontent_values
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+    def view_get(self, view_id=None, view_type='form', **options):
         if view_type == 'form' and self.user_has_groups('hr_recruitment.group_hr_recruitment_interviewer'):
             view_id = self.env.ref('hr_recruitment.hr_applicant_view_form_interviewer').id
-        return super().fields_view_get(view_id, view_type, toolbar, submenu)
+        return super().view_get(view_id, view_type, **options)
 
     def _notify_compute_recipients(self, message, msg_vals):
         """

odoo/addons/test_new_api/tests/test_onchange.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from lxml import etree
 from unittest.mock import patch
 
 from odoo.addons.base.tests.common import SavepointCaseWithUserDemo
@@ -329,7 +330,12 @@ class TestOnChange(SavepointCaseWithUserDemo):
         discussion = self.env.ref('test_new_api.discussion_0')
         demo = self.user_demo
 
-        field_onchange = self.Discussion._onchange_spec()
+        view_info = self.Discussion.view_get()
+        tree = etree.fromstring(view_info['arch'])
+        field_participants = tree.xpath('//field[@name="participants"]')[0]
+        field_participants.remove(field_participants.xpath('./form')[0])
+        view_info['arch'] = etree.tostring(tree)
+        field_onchange = self.Discussion._onchange_spec(view_info=view_info)
         self.assertEqual(field_onchange.get('moderator'), '1')
         self.assertItemsEqual(
             strip_prefix('participants.', field_onchange),
@@ -424,7 +430,7 @@ class TestOnChange(SavepointCaseWithUserDemo):
 
         # mimic UI behaviour, so we get subfields
         # (we need at least subfield: 'important_emails.important')
-        view_info = self.Discussion.fields_view_get(
+        view_info = self.Discussion.view_get(
             view_id=self.env.ref('test_new_api.discussion_form').id,
             view_type='form')
         field_onchange = self.Discussion._onchange_spec(view_info=view_info)

ics-globe/odoo-einvoice-dev

addons/hr/models/res_users.py
@@ -168,7 +168,7 @@ class User(models.Model):
         return super().SELF_WRITEABLE_FIELDS + HR_WRITABLE_FIELDS
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+    def get_view(self, view_id=None, view_type='form', **options):
         # When the front-end loads the views it gets the list of available fields
         # for the user (according to its access rights). Later, when the front-end wants to
         # populate the view with data, it only asks to read those available fields.
@@ -181,11 +181,11 @@ class User(models.Model):
         original_user = self.env.user
         if profile_view and view_id == profile_view.id:
             self = self.with_user(SUPERUSER_ID)
-        result = super(User, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        result = super(User, self).get_view(view_id=view_id, view_type=view_type, **options)
         # Due to using the SUPERUSER the result will contain action that the user may not have access too
         # here we filter out actions that requires special implicit rights to avoid having unusable actions
         # in the dropdown menu.
-        if toolbar and self.env.user != original_user:
+        if options.get('toolbar') and self.env.user != original_user:
             self = self.with_user(original_user.id)
             if not self.user_has_groups("base.group_erp_manager"):
                 change_password_action = self.env.ref("base.change_password_wizard_action")

addons/hr/tests/test_self_user_access.py
@@ -3,6 +3,7 @@
 
 from collections import OrderedDict
 from itertools import chain
+from lxml import etree
 
 from odoo.addons.hr.tests.common import TestHrCommon
 from odoo.tests import new_test_user, tagged, Form
@@ -20,8 +21,8 @@ class TestSelfAccessProfile(TestHrCommon):
             'user_id': james.id,
         })
         view = self.env.ref('hr.res_users_view_form_profile')
-        view_infos = james.fields_view_get(view_id=view.id)
-        fields = view_infos['fields'].keys()
+        view_infos = james.get_view(view_id=view.id)
+        fields = [el.get('name') for el in etree.fromstring(view_infos['arch']).xpath('//field[not(ancestor::field)]')]
         james.read(fields)
 
     def test_readonly_fields(self):
@@ -35,12 +36,12 @@ class TestSelfAccessProfile(TestHrCommon):
         })
 
         view = self.env.ref('hr.res_users_view_form_profile')
-        view_infos = james.fields_view_get(view_id=view.id)
-
+        fields = james._fields
+        view_infos = james.get_view(view_id=view.id)
         employee_related_fields = {
-            field_name
-            for field_name, field_attrs in view_infos['fields'].items()
-            if field_attrs.get('related', (None,))[0] == 'employee_id'
+            el.get('name')
+            for el in etree.fromstring(view_infos['arch']).xpath('//field[not(ancestor::field)]')
+            if fields[el.get('name')].related and fields[el.get('name')].related.split('.')[0] == 'employee_id'
         }
 
         form = Form(james, view=view)
@@ -65,16 +66,16 @@ class TestSelfAccessProfile(TestHrCommon):
             all_groups |= self.env.ref(xml_id.strip())
         user_all_groups = new_test_user(self.env, groups='base.group_user', login='hel', name='God')
         user_all_groups.write({'groups_id': [(4, group.id, False) for group in all_groups]})
-        view_infos = self.env['res.users'].with_user(user_all_groups).fields_view_get(view_id=view.id)
-        full_fields = view_infos['fields']
+        view_infos = self.env['res.users'].with_user(user_all_groups).get_view(view_id=view.id)
+        full_fields = [el.get('name') for el in etree.fromstring(view_infos['arch']).xpath('//field[not(ancestor::field)]')]
 
         # Now check the view for a simple user
         user = new_test_user(self.env, login='gro', name='Grouillot')
-        view_infos = self.env['res.users'].with_user(user).fields_view_get(view_id=view.id)
-        fields = view_infos['fields']
+        view_infos = self.env['res.users'].with_user(user).get_view(view_id=view.id)
+        fields = [el.get('name') for el in etree.fromstring(view_infos['arch']).xpath('//field[not(ancestor::field)]')]
 
         # Compare both
-        self.assertEqual(full_fields.keys(), fields.keys(), "View fields should not depend on user's groups")
+        self.assertEqual(full_fields, fields, "View fields should not depend on user's groups")
 
     def test_access_my_profile_toolbar(self):
         """ A simple user shouldn't have the possibilities to see the 'Change Password' action"""
@@ -85,7 +86,7 @@ class TestSelfAccessProfile(TestHrCommon):
             'user_id': james.id,
         })
         view = self.env.ref('hr.res_users_view_form_profile')
-        available_actions = james.fields_view_get(view_id=view.id, toolbar=True)['toolbar']['action']
+        available_actions = james.get_view(view_id=view.id, toolbar=True)['toolbar']['action']
         change_password_action = self.env.ref("base.change_password_wizard_action")
 
         self.assertFalse(any(x['id'] == change_password_action.id for x in available_actions))
@@ -98,7 +99,7 @@ class TestSelfAccessProfile(TestHrCommon):
             'user_id': john.id,
         })
         view = self.env.ref('hr.res_users_view_form_profile')
-        available_actions = john.fields_view_get(view_id=view.id, toolbar=True)['toolbar']['action']
+        available_actions = john.get_view(view_id=view.id, toolbar=True)['toolbar']['action']
         self.assertTrue(any(x['id'] == change_password_action.id for x in available_actions))
 
 

addons/hr_recruitment/models/hr_recruitment.py
@@ -416,10 +416,10 @@ class Applicant(models.Model):
         return nocontent_body % nocontent_values
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+    def get_view(self, view_id=None, view_type='form', **options):
         if view_type == 'form' and self.user_has_groups('hr_recruitment.group_hr_recruitment_interviewer'):
             view_id = self.env.ref('hr_recruitment.hr_applicant_view_form_interviewer').id
-        return super().fields_view_get(view_id, view_type, toolbar, submenu)
+        return super().get_view(view_id, view_type, **options)
 
     def _notify_compute_recipients(self, message, msg_vals):
         """

odoo/addons/test_new_api/tests/test_onchange.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from lxml import etree
 from unittest.mock import patch
 
 from odoo.addons.base.tests.common import SavepointCaseWithUserDemo
@@ -329,7 +330,12 @@ class TestOnChange(SavepointCaseWithUserDemo):
         discussion = self.env.ref('test_new_api.discussion_0')
         demo = self.user_demo
 
-        field_onchange = self.Discussion._onchange_spec()
+        view_info = self.Discussion.get_view()
+        tree = etree.fromstring(view_info['arch'])
+        field_participants = tree.xpath('//field[@name="participants"]')[0]
+        field_participants.remove(field_participants.xpath('./form')[0])
+        view_info['arch'] = etree.tostring(tree)
+        field_onchange = self.Discussion._onchange_spec(view_info=view_info)
         self.assertEqual(field_onchange.get('moderator'), '1')
         self.assertItemsEqual(
             strip_prefix('participants.', field_onchange),
@@ -424,7 +430,7 @@ class TestOnChange(SavepointCaseWithUserDemo):
 
         # mimic UI behaviour, so we get subfields
         # (we need at least subfield: 'important_emails.important')
-        view_info = self.Discussion.fields_view_get(
+        view_info = self.Discussion.get_view(
             view_id=self.env.ref('test_new_api.discussion_form').id,
             view_type='form')
         field_onchange = self.Discussion._onchange_spec(view_info=view_info)