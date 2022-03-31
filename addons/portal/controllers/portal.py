b2b9e1510f7041266e5ecb14f1423330bfc69225
std-odoo <std@odoo.com>
2022-03-31 11:20:35 +0200

    [FIX] portal: correctly  show the correct error message during password update
    Bug
    ===
    During the password update from the portal view, the error
    "The old password you provided is incorrect" is never displayed.
    
    The reason for that is AccessDenied inherits from UserError, so it
    should be catched before.
    
    Task-2629544

--- addons/portal/controllers/portal.py
+++ addons/portal/controllers/portal.py
@@ -246,8 +246,6 @@ class CustomerPortal(Controller):
 
         try:
             request.env['res.users'].change_password(old, new1)
-        except UserError as e:
-            return {'errors': {'password': e.name}}
         except AccessDenied as e:
             # FIXME XMO dead code since https://github.com/odoo/odoo/pull/45723
             # AccessDenied is an UserError now
@@ -255,6 +253,8 @@ class CustomerPortal(Controller):
             if msg == AccessDenied().args[0]:
                 msg = _('The old password you provided is incorrect, your password was not changed.')
             return {'errors': {'password': {'old': msg}}}
+        except UserError as e:
+            return {'errors': {'password': e.name}}
 
         # update session token so the user does not get logged out (cache cleared by passwd change)
         new_token = request.env.user._compute_session_token(request.session.sid)
