danielosser/odoo

[IMP] web_editor, website: add options to upload documents on links
It is now possible to upload a file via the link tools, and link it to a
button.

An option was added when a link is used for serving a document to
specify how it should be served : either opening it in the browser or
downloading it.

As documents are handled through links, they won't be selectable via the
media dialog opened with "Insert media" or double clicking on an image.

task-2172311

Co-authored-by: Matthieu Stockbauer <tsm@odoo.com>

addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -3216,10 +3216,15 @@ var SnippetsMenu = Widget.extend({
         const $toolbarTableContainer = this.$('#o-we-editor-table-container');
         const docSelection = document.getSelection();
         const $currentSelectionTarget = docSelection && docSelection.rangeCount > 0 ? $(docSelection.getRangeAt(0).commonAncestorContainer) : $();
-        // Do not  toggle visibility if the target is inside the toolbar ( eg. during link edition).
-        if ($currentSelectionTarget.closest('#o_we_editor_toolbar_container').length ||
-            (e && $(e.target).closest('#o_we_editor_toolbar_container').length)
-        ) {
+        const isClickOnToolbar = $currentSelectionTarget.closest('#o_we_editor_toolbar_container').length
+            || (e && $(e.target).closest('#o_we_editor_toolbar_container').length);
+        // The selector .o_existing_attachment_cell is still needed as the
+        // media dialog will already be destroyed at that point.
+        const isClickOnMediaDialog = (e && e.target.closest('.o_select_media_dialog, .o_existing_attachment_cell'));
+
+        // Do not toggle visibility if the target is inside the toolbar
+        // (eg. during link edition), or inside the media selection dialog.
+        if (isClickOnToolbar || isClickOnMediaDialog) {
             return;
         }
 

addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -911,8 +911,7 @@ const Wysiwyg = Widget.extend({
                 }
                 const _onMousedown = ev => {
                     if (
-                        !ev.target.closest('.oe-toolbar') &&
-                        !ev.target.closest('.ui-autocomplete') &&
+                        !ev.target.closest('.oe-toolbar, .ui-autocomplete, .o_select_media_dialog') &&
                         (!this.linkTools || ![ev.target, ...wysiwygUtils.ancestors(ev.target)].includes(this.linkTools.$link[0]))
                     ) {
                         // Destroy the link tools on click anywhere outside the

ics-globe/odoo-einvoice-dev

addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -3325,10 +3325,15 @@ var SnippetsMenu = Widget.extend({
         const $toolbarTableContainer = this.$('#o-we-editor-table-container');
         const docSelection = document.getSelection();
         const $currentSelectionTarget = docSelection && docSelection.rangeCount > 0 ? $(docSelection.getRangeAt(0).commonAncestorContainer) : $();
-        // Do not  toggle visibility if the target is inside the toolbar ( eg. during link edition).
-        if ($currentSelectionTarget.closest('#o_we_editor_toolbar_container').length ||
-            (e && $(e.target).closest('#o_we_editor_toolbar_container').length)
-        ) {
+        const isClickOnToolbar = $currentSelectionTarget.closest('#o_we_editor_toolbar_container').length
+            || (e && $(e.target).closest('#o_we_editor_toolbar_container').length);
+        // The selector .o_existing_attachment_cell is still needed as the
+        // media dialog will already be destroyed at that point.
+        const isClickOnMediaDialog = (e && e.target.closest('.o_technical_modal, .o_existing_attachment_cell'));
+
+        // Do not toggle visibility if the target is inside the toolbar
+        // (eg. during link edition), or inside the media selection dialog.
+        if (isClickOnToolbar || isClickOnMediaDialog) {
             return;
         }
 

addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1039,8 +1039,7 @@ const Wysiwyg = Widget.extend({
                 this.linkTools.noFocusUrl = options.noFocusUrl;
                 const _onMousedown = ev => {
                     if (
-                        !ev.target.closest('.oe-toolbar') &&
-                        !ev.target.closest('.ui-autocomplete') &&
+                        !ev.target.closest('.oe-toolbar, .ui-autocomplete, .o_technical_modal') &&
                         (!this.linkTools || ![ev.target, ...wysiwygUtils.ancestors(ev.target)].includes(this.linkTools.$link[0]))
                     ) {
                         // Destroy the link tools on click anywhere outside the