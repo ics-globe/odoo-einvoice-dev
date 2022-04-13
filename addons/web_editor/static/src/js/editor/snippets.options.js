531b5780176302c75baefe4c26c4ff8a639e9636
Nicolas Bayet <nby@odoo.com>
2022-04-13 10:58:18 +0200

    [FIX] web_editor: patch jquery sortable to work with iframe
    Before this commit, trying to drag&drop using jquery sortable
    in mass mailing did not work.
    The jquery sortable is used in the `ListUserValueWidget`.
    The `ListUserValueWidget` is used in the `SocialMedia` snippet
    option.
    The reason was:
    1. The iframe prevented the `window` main event to fire.
    2. The position of the dragged element was not accurate.
    
    task-2739302

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -32,6 +32,26 @@ const {
 var qweb = core.qweb;
 var _t = core._t;
 
+// Patch ui.sortable to take into consideration the iframe of the wysiwyg.
+$.widget( "ui.sortable", $.ui.sortable, {
+    _generatePosition: function( event ) {
+        const pos = this._super(event);
+        if (this.options.wysiwygIframe) {
+            const offset = this.options.wysiwygIframe.offset();
+            pos.top -= offset.top;
+            pos.left -= offset.left;
+        }
+        return pos;
+    },
+    _mouseDistanceMet: function(...args) {
+        if (this.options.wysiwygIframe) {
+            return true;
+        } else {
+            return this._super(...args);
+        }
+    },
+});
+
 /**
  * @param {HTMLElement} el
  * @param {string} [title]
@@ -2090,13 +2110,24 @@ const ListUserValueWidget = UserValueWidget.extend({
         if (this.el.dataset.unsortable) {
             return;
         }
+        const wysiwyg = this.getParent().options.wysiwyg;
+
         $(this.listTable).sortable({
             axis: 'y',
             handle: '.o_we_drag_handle',
             items: 'tr',
             cursor: 'move',
             opacity: 0.6,
-            stop: (event, ui) => {
+            wysiwygIframe: wysiwyg && wysiwyg.$iframe,
+            start: () => {
+                if (wysiwyg && wysiwyg.$iframe) {
+                    wysiwyg.$iframe.css('pointer-events', 'none');
+                }
+            },
+            stop: () => {
+                if (wysiwyg && wysiwyg.$iframe) {
+                    wysiwyg.$iframe.css('pointer-events', '');
+                }
                 this._notifyCurrentState();
             },
         });
