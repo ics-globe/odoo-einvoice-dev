76e00e343ded919c72d0eb1ae602138ca7297f1b
Nasreddin Boulif (bon) <bon@odoo.com>
2022-04-15 16:06:32 +0200

    [FIX] event_booth_sale: Allow confirm sale order with event booth
    Steps to Reproduce:
    
      - Connect as Admin
      - Install event_booth_sale module
      - Give only sales right to Demo user and remove event rights
      - Connect as Demo
      - Create a new SO
      - Add an event booth as product to the SO
      - Try to confirm the SO
    
    Issue:
    
      Access error.
    
    Cause:
    
      When comfirming the SO, we also update the selected event_booth
      while sales rights are not enough to update event_booth model.
    
    Solution:
    
      Use sudo to update event_booth.
    
    opw-2823555

--- addons/event_booth_sale/tests/test_event_booth_sale.py
+++ addons/event_booth_sale/tests/test_event_booth_sale.py
@@ -3,9 +3,9 @@
 
 from odoo import Command
 from odoo.addons.event_booth_sale.tests.common import TestEventBoothSaleCommon
-from odoo.tests.common import users
+from odoo.addons.sale.tests.common import TestSaleCommon
+from odoo.tests.common import tagged, users
 from odoo.tools import float_compare
-
 
 class TestEventBoothSale(TestEventBoothSaleCommon):
 
@@ -72,3 +72,66 @@ class TestEventBoothSale(TestEventBoothSaleCommon):
                          "Untaxed amount should be the sum of the booths prices ($200.0).")
         self.assertEqual(float_compare(sale_order.amount_total, 220.0, precision_rounding=0.1), 0,
                          "Total amount should be the sum of the booths prices with 10% taxes ($200.0 + $20.0).")
+
+        # Confirm the SO.
+        sale_order.action_confirm()
+
+@tagged('post_install', '-at_install')
+class TestEventBoothSaleInvoice(TestSaleCommon, TestEventBoothSaleCommon):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestEventBoothSaleInvoice, cls).setUpClass()
+
+        cls.booth_1 = cls.env['event.booth'].create({
+            'name': 'Test Booth 1',
+            'booth_category_id': cls.event_booth_category_1.id,
+            'event_id': cls.event_0.id,
+        })
+
+        cls.tax_10 = cls.env['account.tax'].sudo().create({
+            'name': 'Tax 10',
+            'amount': 10,
+        })
+
+        cls.pricelist = cls.env['product.pricelist'].sudo().create({
+            'name': 'Test Pricelist',
+        })
+
+    @users('user_sales_salesman')
+    def test_event_booth_with_invoice(self):
+        # Add group `group_account_invoice` to user to allow to pay the invoice
+        self.env.user.groups_id += self.env.ref('account.group_account_invoice')
+
+        self.event_booth_product.taxes_id = self.tax_10
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.event_customer.id,
+            'pricelist_id': self.pricelist.id,
+            'order_line': [
+                Command.create({
+                    'product_id': self.event_booth_product.id,
+                    'event_id': self.event_0.id,
+                    'event_booth_pending_ids': (self.booth_1).ids
+                })
+            ]
+        })
+        sale_order.action_confirm()
+
+        # Create and check that the invoice was created
+        invoice = sale_order._create_invoices()
+        self.assertEqual(len(sale_order.invoice_ids), 1, "Invoice not created.")
+
+        # Confirm the invoice and check SO invoice status
+        invoice.action_post()
+        self.assertTrue(sale_order.invoice_status == 'invoiced', 'order is not invoiced')
+
+        # Pay the invoice.
+        journal = self.env['account.journal'].search([('type', '=', 'cash'), ('company_id', '=', sale_order.company_id.id)], limit=1)
+
+        register_payments = self.env['account.payment.register'].with_context(active_model='account.move', active_ids=invoice.ids).create({
+            'journal_id': journal.id,
+        })
+        register_payments._create_payments()
+
+        # Check the invoice payment state after paying the invoice
+        self.assertTrue(invoice.payment_state == 'paid', 'order is not paid')
