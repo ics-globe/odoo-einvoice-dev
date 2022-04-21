ddc488af1598d22d9eb9f80d50dd9037129f24eb
Vincent Schippefilt <vsc@odoo.com>
2022-04-21 13:36:06 +0200

    [IMP] base: unused fields in ir_module_dependency
    The table ir_module_dependency was created manually with tracking fields,
    but the CRUD was done manually, without the ORM.
    Removing the fields that are always null will not have any impact
    whatsoever but we should still do it to be correct.

--- odoo/addons/base/models/ir_module.py
+++ odoo/addons/base/models/ir_module.py
@@ -1020,6 +1020,7 @@ DEP_STATES = STATES + [('unknown', 'Unknown')]
 class ModuleDependency(models.Model):
     _name = "ir.module.module.dependency"
     _description = "Module dependency"
+    _log_access = False  # inserts are done manually, create and write uid, dates are always null
 
     # the dependency name
     name = fields.Char(index=True)
