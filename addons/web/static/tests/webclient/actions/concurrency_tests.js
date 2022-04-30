odoo/odoo

[WIP] duplicate and unskip the last concurrency test

addons/web/static/tests/webclient/actions/concurrency_tests.js
@@ -549,9 +549,7 @@ QUnit.module("ActionManager", (hooks) => {
         ]);
     });
 
-    QUnit.skip("click multiple times to open a record", async function (assert) {
-        assert.expect(5);
-
+    QUnit.test("click multiple times to open a record", async function (assert) {
         const def = makeDeferred();
         const defs = [null, def];
         const mockRPC = async (route, args) => {
@@ -570,13 +568,21 @@ QUnit.module("ActionManager", (hooks) => {
         await click(webClient.el.querySelector(".o_back_button"));
         assert.containsOnce(webClient, ".o_list_view");
 
-        await click(webClient.el.querySelector(".o_list_view .o_data_row"));
-        await click(webClient.el.querySelector(".o_list_view .o_data_row"));
-        assert.containsOnce(webClient, ".o_list_view");
+        await click(webClient.el.querySelectorAll(".o_list_view .o_data_row")[0]);
+        await click(webClient.el.querySelectorAll(".o_list_view .o_data_row")[1]);
+        assert.containsOnce(webClient, ".o_form_view");
+        assert.strictEqual(
+            webClient.el.querySelector(".breadcrumb-item.active").innerText,
+            "Second record"
+        );
 
         def.resolve();
         await nextTick();
         assert.containsOnce(webClient, ".o_form_view");
+        assert.strictEqual(
+            webClient.el.querySelector(".breadcrumb-item.active").innerText,
+            "Second record"
+        );
     });
 
     QUnit.test("local state, global state, and race conditions", async function (assert) {

ics-globe/odoo-einvoice-dev

addons/web/static/tests/webclient/actions/concurrency_tests.js
@@ -598,9 +598,7 @@ QUnit.module("ActionManager", (hooks) => {
         ]);
     });
 
-    QUnit.skip("click multiple times to open a record", async function (assert) {
-        assert.expect(5);
-
+    QUnit.test("click multiple times to open a record", async function (assert) {
         const def = makeDeferred();
         const defs = [null, def];
         const mockRPC = async (route, args) => {
@@ -619,13 +617,21 @@ QUnit.module("ActionManager", (hooks) => {
         await click(target.querySelector(".o_back_button"));
         assert.containsOnce(target, ".o_list_view");
 
-        await click(target.querySelector(".o_list_view .o_data_row"));
-        await click(target.querySelector(".o_list_view .o_data_row"));
-        assert.containsOnce(target, ".o_list_view");
+        await click(target.querySelectorAll(".o_list_view .o_data_row")[0]);
+        await click(target.querySelectorAll(".o_list_view .o_data_row")[1]);
+        assert.containsOnce(target, ".o_form_view");
+        assert.strictEqual(
+            target.querySelector(".breadcrumb-item.active").innerText,
+            "Second record"
+        );
 
         def.resolve();
         await nextTick();
         assert.containsOnce(target, ".o_form_view");
+        assert.strictEqual(
+            target.querySelector(".breadcrumb-item.active").innerText,
+            "Second record"
+        );
     });
 
     QUnit.test("local state, global state, and race conditions", async function (assert) {