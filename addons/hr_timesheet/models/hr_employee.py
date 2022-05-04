d5d46d1931b1111b818f5f8834e2ec0e0db6cc6f
Krina Oza <kro@odoo.com>
2022-05-04 11:58:43 +0530

    [IMP] timesheets

--- addons/hr_timesheet/models/hr_employee.py
+++ addons/hr_timesheet/models/hr_employee.py
@@ -10,3 +10,11 @@ class HrEmployee(models.Model):
     timesheet_cost = fields.Monetary('Cost', currency_field='currency_id',
     	groups="hr.group_hr_user", default=0.0)
     currency_id = fields.Many2one('res.currency', related='company_id.currency_id', readonly=True)
+
+    def name_get(self):
+        res = super().name_get()
+        name = dict(res)
+        for employee in self:
+            if len(employee.user_id.company_ids) > 1 and len(self.env.context.get('allowed_company_ids', [])) > 1:
+                name[employee.id] = f'{name[employee.id]} ({employee.company_id.name})'
+        return list(name.items())
