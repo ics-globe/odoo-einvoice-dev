a6bec6c34d712669f8db29ddf13cbc9a0bb387e6
Younn Olivier <yol@odoo.com>
2022-05-10 10:51:50 +0200

    [IMP] website, *: add assets_media_dialog and use assets_editor
    This commit fixes two issues related to the loading the wysiwyg and
    editor assets.
    
    The media dialog components are now defined in a new assets_media_dialog
    bundle, to make them available both in the frontend (needed to post
    comments on the forum for example), and in the backend (used in the
    context of the wysiwyg edition or in the seo dialog). Ideally, The media
    dialog component would lazy load its own bundle, but right now, it is
    not possible when used in a ComponentWrapper.
    
    The wysiwyg assets are now completely loaded in the frontend, after
    creating the website root, when the frontend is displayed in an iframe.
    It would be better if the frontend loads only what it needs of the
    wysiwyg assets (some editor classes and the drop zones css), an
    improvement would be to create an assets_wysiwyg_frontend or
    assets_wysiwyg_minimal bundle.
    
    The website edition components are moved to the assets_editor bundle.
    For now, this bundle is included in the backend and the edition menus
    are hidden from the users that are not website publishers. Ideally, the
    client action would lazy load the assets_editor, only if the user is a
    website publisher.
    
    task-2687506

--- addons/website/static/src/client_actions/website_editor/website_editor.js
+++ addons/website/static/src/client_actions/website_editor/website_editor.js
@@ -16,6 +16,7 @@ export class WebsiteEditorClientAction extends Component {
         this.websiteService = useService('website');
         this.dialogService = useService('dialog');
         this.title = useService('title');
+        this.user = useService('user');
 
         this.iframeFallbackUrl = '/website/iframefallback';
 
@@ -24,6 +25,7 @@ export class WebsiteEditorClientAction extends Component {
         this.websiteContext = useState(this.websiteService.context);
 
         onWillStart(async () => {
+            this.isWebsitePublisher = await this.user.hasGroup('website.group_website_publisher');
             await this.websiteService.fetchWebsites();
             this.initialUrl = `/website/force/${this.websiteId}?path=${this.path}`;
         });
@@ -126,12 +128,14 @@ export class WebsiteEditorClientAction extends Component {
     }
 
     addWelcomeMessage() {
-        const $wrap = $(this.iframe.el.contentDocument.getElementById('wrap'));
-        if ($wrap.length && $wrap.html().trim() === '') {
-            this.$welcomeMessage = $(core.qweb.render('website.homepage_editor_welcome_message'));
-            this.$welcomeMessage.addClass('o_homepage_editor_welcome_message');
-            this.$welcomeMessage.css('min-height', $wrap.parent('main').height() - ($wrap.outerHeight(true) - $wrap.height()));
-            $wrap.empty().append(this.$welcomeMessage);
+        if (this.isWebsitePublisher) {
+            const $wrap = $(this.iframe.el.contentDocument.getElementById('wrap'));
+            if ($wrap.length && $wrap.html().trim() === '') {
+                this.$welcomeMessage = $(core.qweb.render('website.homepage_editor_welcome_message'));
+                this.$welcomeMessage.addClass('o_homepage_editor_welcome_message');
+                this.$welcomeMessage.css('min-height', $wrap.parent('main').height() - ($wrap.outerHeight(true) - $wrap.height()));
+                $wrap.empty().append(this.$welcomeMessage);
+            }
         }
     }
 }
--- addons/website/static/src/services/website_service.js
+++ addons/website/static/src/services/website_service.js
@@ -102,9 +102,6 @@ export const websiteService = {
             get editedObjectPath() {
                 return editedObjectPath;
             },
-            get wysiwygLoaded() {
-                return !!Wysiwyg;
-            },
             goToWebsite({ websiteId, path, edition, translation } = {}) {
                 action.doAction('website.website_editor', {
                     clearBreadcrumbs: true,
