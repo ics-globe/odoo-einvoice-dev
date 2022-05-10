92af91b55913fbe04bb424358d0376ded35da808
smdc-odoo <smdc@odoo.com>
2022-05-10 15:05:01 +0200

    Add a default customer (set by the user in the config) to all order without a customer

--- addons/l10n_mx_pos/models/pos_session.py
+++ addons/l10n_mx_pos/models/pos_session.py
@@ -7,3 +7,13 @@ class PosSession(models.Model):
         res = super(PosSession, self)._accumulate_amounts(data)
         print('ok')
         return res
+
+    def _validate_session(self, balancing_account=False, amount_to_balance=0, bank_payment_method_diffs=None):
+        res = super(PosSession, self)._validate_session(balancing_account, amount_to_balance, bank_payment_method_diffs)
+        orders_without_customer = self.env['pos.order'].search([('session_id', '=', f'{self.name}'), ('partner_id', '=', None)])
+        general_customer = self.env.ref('l10n_mx_pos.res_partner_general_customer_mx')
+        for order in orders_without_customer:
+            order.write({
+                'partner_id': general_customer
+            })
+        return res
