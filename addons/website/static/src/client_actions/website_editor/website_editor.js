yourjelly/odoo

[IMP] web_editor, website: use master's loading spinner
Use a blockUI component when the iframe is being reloaded,
use the new loading spinner in the web_editor.
Scroll the snippet into view when reloading the editor.

addons/website/static/src/client_actions/website_editor/website_editor.js
@@ -2,6 +2,7 @@
 
 import { registry } from '@web/core/registry';
 import { useService } from '@web/core/utils/hooks';
+import { BlockUI } from "@web/core/ui/block_ui";
 import core from 'web.core';
 
 import { WebsiteEditorComponent } from '../../components/editor/editor';
@@ -48,7 +49,6 @@ export class WebsiteEditorClientAction extends Component {
                         $().getScrollingElement(this.iframefallback.el.contentDocument)[0].scrollTop = $().getScrollingElement(this.iframe.el.contentDocument)[0].scrollTop;
                     }
                 });
-
                 this.iframe.el.contentWindow.addEventListener('PUBLIC-ROOT-READY', (event) => {
                     if (!this.websiteContext.edition) {
                         this.addWelcomeMessage();
@@ -70,6 +70,11 @@ export class WebsiteEditorClientAction extends Component {
                 this.addWelcomeMessage();
             }
         }, () => [this.websiteContext.edition]);
+
+        useEffect(() => {
+            this.websiteService.blockIframe();
+            this.iframe.el.addEventListener('load', () => this.websiteService.unblockIframe(), { once: true });
+        }, () => []);
     }
 
     get websiteId() {
@@ -117,6 +122,6 @@ export class WebsiteEditorClientAction extends Component {
     }
 }
 WebsiteEditorClientAction.template = 'website.WebsiteEditorClientAction';
-WebsiteEditorClientAction.components = { WebsiteEditorComponent };
+WebsiteEditorClientAction.components = { WebsiteEditorComponent, BlockUI };
 
 registry.category('actions').add('website_editor', WebsiteEditorClientAction);

ics-globe/odoo-einvoice-dev

addons/website/static/src/client_actions/website_editor/website_editor.js
@@ -2,6 +2,7 @@
 
 import { registry } from '@web/core/registry';
 import { useService } from '@web/core/utils/hooks';
+import { BlockUI } from "@web/core/ui/block_ui";
 import core from 'web.core';
 
 import { WebsiteEditorComponent } from '../../components/editor/editor';
@@ -47,7 +48,6 @@ export class WebsiteEditorClientAction extends Component {
                         $().getScrollingElement(this.iframefallback.el.contentDocument)[0].scrollTop = $().getScrollingElement(this.iframe.el.contentDocument)[0].scrollTop;
                     }
                 });
-
                 this.iframe.el.contentWindow.addEventListener('PUBLIC-ROOT-READY', (event) => {
                     if (!this.websiteContext.edition) {
                         this.addWelcomeMessage();
@@ -69,6 +69,11 @@ export class WebsiteEditorClientAction extends Component {
                 this.addWelcomeMessage();
             }
         }, () => [this.websiteContext.edition]);
+
+        useEffect(() => {
+            this.websiteService.blockIframe();
+            this.iframe.el.addEventListener('OdooFrameContentLoaded', () => this.websiteService.unblockIframe(), { once: true });
+        }, () => []);
     }
 
     get websiteId() {
@@ -116,6 +121,6 @@ export class WebsiteEditorClientAction extends Component {
     }
 }
 WebsiteEditorClientAction.template = 'website.WebsiteEditorClientAction';
-WebsiteEditorClientAction.components = { WebsiteEditorComponent };
+WebsiteEditorClientAction.components = { WebsiteEditorComponent, BlockUI };
 
 registry.category('actions').add('website_editor', WebsiteEditorClientAction);