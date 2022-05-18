boan-odoo/odoo-1

[IMP] event_sale: add event revenue reporting
As an event manager, it can be difficult to estimate how much of the revenue
was generated by the events. To give clear insights, we will add new reports
to the 'event' module.

With those new reports, the user can group the revenues by event (type),
ticket, product, etc. The user will then be able to quickly see which tickets
and events are the most profitable ones. The reports will be accessible only if
the user installed the 'sale' module.

Task-2679881
See odoo/enterprise#24472

Co-authored-by: Julien Banken <jbn@odoo.com>

addons/event_sale/__manifest__.py
@@ -22,6 +22,7 @@ this event.
     'data': [
         'views/event_ticket_views.xml',
         'views/event_registration_views.xml',
+        'views/event_sale_report_views.xml',
         'views/event_views.xml',
         'views/sale_order_views.xml',
         'data/event_sale_data.xml',
@@ -32,7 +33,11 @@ this event.
         'wizard/event_edit_registration.xml',
         'wizard/event_configurator_views.xml',
     ],
-    'demo': ['data/event_demo.xml'],
+    'demo': [
+        'data/event_sale_demo.xml',
+        'data/event_demo.xml',  # needs event_sale_demo
+        'data/event_registration_demo.xml',  # needs event_sale_demo
+    ],
     'installable': True,
     'auto_install': True,
     'assets': {

addons/website_event_sale/__manifest__.py
@@ -12,6 +12,7 @@ Sell event tickets through eCommerce app.
     'data': [
         'data/event_data.xml',
         'views/event_event_views.xml',
+        'views/event_sale_report_views.xml',
         'views/website_event_templates.xml',
         'views/website_sale_templates.xml',
         'security/website_event_sale_security.xml',

ics-globe/odoo-einvoice-dev

Co-authored-by: Florian Charlier <flch@odoo.com>

addons/event_sale/__manifest__.py
@@ -27,12 +27,18 @@ this event.
         'data/event_sale_data.xml',
         'data/mail_data.xml',
         'report/event_event_templates.xml',
+        'report/event_sale_report_views.xml',
         'security/ir.model.access.csv',
+        'security/ir_rule.xml',
         'security/event_security.xml',
         'wizard/event_edit_registration.xml',
         'wizard/event_configurator_views.xml',
     ],
-    'demo': ['data/event_demo.xml'],
+    'demo': [
+        'data/event_sale_demo.xml',
+        'data/event_demo.xml',  # needs event_sale_demo
+        'data/event_registration_demo.xml',  # needs event_sale_demo
+    ],
     'installable': True,
     'auto_install': True,
     'assets': {

addons/website_event_sale/__manifest__.py
@@ -11,6 +11,7 @@ Sell event tickets through eCommerce app.
     'depends': ['website_event', 'event_sale', 'website_sale'],
     'data': [
         'data/event_data.xml',
+        'report/event_sale_report_views.xml',
         'views/event_event_views.xml',
         'views/website_event_templates.xml',
         'views/website_sale_templates.xml',