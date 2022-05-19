f2847634dcc4cc213419c5c9ed80caf6d362fce4
Myriam (myra) <myra@odoo.com>
2022-05-19 15:04:31 +0200

    [FEAT] l10n_co: Menu item.

--- addons/l10n_co/__manifest__.py
+++ addons/l10n_co/__manifest__.py
@@ -22,20 +22,20 @@
     ],
     'data': [
         # Chart of Accounts
+        'data/account_chart_template_data.xml',
         'data/account.account.template.csv',
         'data/account.group.template.csv',
-        'data/account_chart_template_data.xml',
 
         # Taxes
         'data/account.tax.group.csv',
         'data/account_tax_template.xml',
         'data/l10n_latam.identification.type.csv',
 
-        #'data/l10n_co_chart_data.xml',
         'data/account_chart_post_data.xml',
-        'data/account_chart_template_configure_data.xml',
+        'data/account_chart_template_try_loading.xml',
 
         # Views
+        'views/menuitem_data.xml'
     ],
     'demo': [
         'demo/demo_company.xml',
