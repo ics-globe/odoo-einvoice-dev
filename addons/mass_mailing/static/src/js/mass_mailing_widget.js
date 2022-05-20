odoo/odoo

commit 95754eb6a464894417e8f763ae0fc4933ca3a157

[FIX] mass_mailing: save transpiled html without affecting body_arch
Since the introduction of Odoo Editor, `field_html`'s `_getValue` uses
`wysiwyg.getValue`. This conflicts with the mechanism of `mass_mailing`
which modifies the html of the editable area in place to save an
"e-mailable" version on the `body_html` field, then relies on
`_getValue` to reset the original html (`body_arch`).
This ensures the field has a value before modifying the html in place,
so we can manually restore it instead of relying on a side effect of the
old behavior of `_getValue`.

Part-of: odoo/odoo#75901

addons/mass_mailing/static/src/js/mass_mailing_widget.js
@@ -65,6 +65,7 @@ var MassMailingFieldHtml = FieldHtml.extend({
         }
         return this.wysiwyg.saveModifiedImages(this.$content).then(function () {
             self._isDirty = self.wysiwyg.isDirty();
+            self._doAction();
 
             convertInline.attachmentThumbnailToLinkImg($editable);
             convertInline.fontToImg($editable);
@@ -84,6 +85,7 @@ var MassMailingFieldHtml = FieldHtml.extend({
                 changes: _.object([fieldName], [self._unWrap($editable.html())])
             });
 
+            $editable.html(self.value);
             if (self._isDirty && self.mode === 'edit') {
                 return self._doAction();
             }

addons/mass_mailing/static/src/js/tours/mass_mailing_editor_tour.js
@@ -1,0 +1,28 @@
+odoo.define('mass_mailing.mass_mailing_editor_tour', function (require) {
+    "use strict";
+
+    var tour = require('web_tour.tour');
+
+    tour.register('mass_mailing_editor_tour', {
+        url: '/web',
+        test: true,
+    }, [tour.stepUtils.showAppsMenuItem(), {
+        trigger: '.o_app[data-menu-xmlid="mass_mailing.mass_mailing_menu_root"]',
+    }, {
+        trigger: 'button.o_list_button_add',
+    }, {
+        trigger: 'input[name="subject"]',
+        run: 'text Test',
+    }, {
+        trigger: 'div[name="contact_list_ids"] .o_input_dropdown > input[type="text"]',
+    }, {
+        trigger: 'li.ui-menu-item',
+    }, {
+        trigger: 'iframe #default',
+        run: () => $('iframe').contents().find('o_editable').html('<p><br/></p>'),
+    }, {
+        trigger: 'button.o_form_button_save',
+    }, {
+        trigger: 'iframe.o_readonly',
+    }]);
+});

addons/mass_mailing/tests/__init__.py
@@ -7,3 +7,4 @@ from . import test_mailing_internals
 from . import test_mailing_list
 from . import test_mailing_controllers
 from . import test_mailing_mailing_schedule_date
+from . import test_mailing_ui

addons/mass_mailing/tests/test_mailing_ui.py
@@ -1,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import odoo.tests
+from odoo.addons.base.tests.common import HttpCaseWithUserDemo
+
+
+@odoo.tests.tagged('-at_install', 'post_install')
+class TestUi(HttpCaseWithUserDemo):
+    def setUp(self):
+        super().setUp()
+        self.user_demo.groups_id |= self.env.ref('mass_mailing.group_mass_mailing_user')
+        self.user_demo.groups_id |= self.env.ref('mail.group_mail_template_editor')
+
+    def test_01_mass_mailing_editor_tour(self):
+        self.start_tour("/web", 'mass_mailing_editor_tour', login="demo")
+        mail = self.env['mailing.mailing'].search([('subject', '=', 'Test')])[0]
+        # The tour created and saved an email. The edited version should be
+        # saved in body_arch, and its transpiled version (see convert_inline)
+        # for email client compatibility should be saved in body_html. This
+        # ensures both fields have different values.
+        self.assertEqual(mail.body_arch, '<p><br></p>')
+        self.assertEqual(mail.body_html, '<p style="margin:0px 0 1rem 0;"><br></p>')

addons/web_editor/static/src/js/backend/convert_inline.js
@@ -24,7 +24,7 @@ function getMatchedCSSRules(a) {
             try {
                 rules = sheets[i].rules || sheets[i].cssRules;
             } catch (e) {
-                console.warn("Can't read the css rules of: " + sheets[i].href, e);
+                console.log("Can't read the css rules of: " + sheets[i].href, e);
                 continue;
             }
             if (rules) {

ics-globe/odoo-einvoice-dev

commit 8c9c0f4cbeb1297b399c5621551c4c8e5faa7d19

[FIX] mass_mailing: keep body of invalid saved mailing
- Impacted versions

  15.0 and higher

- How to reproduce

  Create a mailing "Start from scratch" and add some elements
  do not fill in the subject or contact_list_ids
  save -> this fails because the fields are required

- Current behavior

  The edited template is lost.

- Expected behavior

  Keep the edited template even if the save failed

- Fix

  Properly await the promise _doAction to set the value of the field

- Tests

  Updated the mass_mailing_editor_tour to test a failed save on an edited
  mailing list

- Origin

  The incorrect behavior was introduced with:
  95754eb6a464894417e8f763ae0fc4933ca3a157

Task-2856742

addons/mass_mailing/static/src/js/mass_mailing_widget.js
@@ -49,7 +49,6 @@ var MassMailingFieldHtml = FieldHtml.extend({
      * @override
      */
     commitChanges: async function () {
-        var self = this;
         if (this.mode === 'readonly' || !this.isRendered) {
             return this._super();
         }
@@ -61,23 +60,23 @@ var MassMailingFieldHtml = FieldHtml.extend({
 
         var $editable = this.wysiwyg.getEditable();
         await this.wysiwyg.cleanForSave();
-        return this.wysiwyg.saveModifiedImages(this.$content).then(function () {
-            self._isDirty = self.wysiwyg.isDirty();
-            self._doAction();
+        return this.wysiwyg.saveModifiedImages(this.$content).then(async () => {
+            this._isDirty = this.wysiwyg.isDirty();
+            await this._doAction();
 
             const $editorEnable = $editable.closest('.editor_enable');
             $editorEnable.removeClass('editor_enable');
-            convertInline.toInline($editable, self.cssRules, self.wysiwyg.$iframe);
+            convertInline.toInline($editable, this.cssRules, this.wysiwyg.$iframe);
             $editorEnable.addClass('editor_enable');
 
-            self.trigger_up('field_changed', {
-                dataPointID: self.dataPointID,
-                changes: _.object([fieldName], [self._unWrap($editable.html())])
+            this.trigger_up('field_changed', {
+                dataPointID: this.dataPointID,
+                changes: _.object([fieldName], [this._unWrap($editable.html())])
             });
 
-            $editable.html(self.value);
-            if (self._isDirty && self.mode === 'edit') {
-                return self._doAction();
+            $editable.html(this.value);
+            if (this._isDirty && this.mode === 'edit') {
+                return this._doAction();
             }
         });
     },

addons/mass_mailing/static/src/js/tours/mass_mailing_editor_tour.js
@@ -11,15 +11,40 @@ odoo.define('mass_mailing.mass_mailing_editor_tour', function (require) {
     }, {
         trigger: 'button.o_list_button_add',
     }, {
-        trigger: 'input[name="subject"]',
-        run: 'text Test',
-    }, {
         trigger: 'div[name="contact_list_ids"] .o_input_dropdown > input[type="text"]',
     }, {
         trigger: 'li.ui-menu-item',
     }, {
-        trigger: 'iframe #default',
-        run: () => $('iframe').contents().find('o_editable').html('<p><br/></p>'),
+        trigger: '[name="body_arch"] iframe #empty',
+    }, {
+        trigger: '[name="body_arch"] iframe .o_editable'
+    }, {
+        trigger: '[name="body_arch"] iframe #email_designer_default_body [name="Title"] .ui-draggable-handle',
+        run: function (actions) {
+            actions.drag_and_drop('[name="body_arch"] iframe .o_editable', this.$anchor);
+        }
+    }, {
+        trigger: '[name="body_arch"] iframe .o_editable h1'
+    }, {
+        trigger: 'button.o_form_button_save',
+    }, {
+        trigger: 'label.o_field_invalid',
+    }, {
+        trigger: '[name="body_arch"] iframe .o_editable h1'
+    }, {
+        trigger: '[name="body_arch"] iframe .odoo-editor-editable',
+        run: function () {
+            const ownerDocument = this.$anchor[0].ownerDocument;
+            const paragraph = ownerDocument.createElement('p');
+            const linebreak = ownerDocument.createElement('br');
+            paragraph.append(linebreak);
+            const fragment = new DocumentFragment();
+            fragment.append(paragraph);
+            this.$anchor[0].replaceChildren(...fragment.childNodes);
+        }
+    }, {
+        trigger: 'input[name="subject"]',
+        run: 'text Test',
     }, {
         trigger: 'button.o_form_button_save',
     }, {